c6d1b5a2a81cfe781a13c6784ebd01f9d7952848
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-24 20:47:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog 
+  attr_accessor :name, :save
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each {|dogs| puts dogs.name}
+    #why is it dogs.name and not "#{name}"? Isn't name an instance variable and able to be used here?
+  end
+  
+end
