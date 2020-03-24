035b8b0d0e114907359cccab7d2eb2cd8e39a2a8
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-24 19:36:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,28 @@
-# Add your code here
+class Dog 
+  attr_accessor :name, :save
+  @@all = []
+  #DOG
+
+  def initialize(name, save)
+    @name = name
+    @save = save
+    @@all << save
+  end
+  
+  def save
+    @@all << self.all
+  end 
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    #operates on the existing dogs and empties that array. use Array#clear method.
+  end
+  
+  def self.print_all
+   # @@all.each {|dogs| puts "#{name}"}
+  end
+  
+end
